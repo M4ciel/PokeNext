bd9c3fb6b0d0d10e4fd83b1b3a4a167abe32d5e2
Caio Maciel <caiolmaciell@gmail.com>
2023-05-03 04:43:04 -0300

    Aula 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
 import styles from "@/styles/Home.module.css";
 
-export default function Home() {
-    return <h1>PokeNext</h1>;
+export async function getStaticProps() {
+    const maxPokemons = 251;
+
+    const api = "https://pokeapi.co/api/v2/pokemon/";
+
+    const res = await fetch(`${api}/?limit=${maxPokemons}`);
+    const data = await res.json();
+
+    data.results.map((item: any, index: number) => {
+        item.id = index + 1;
+    });
+
+    return { props: { pokemons: data.results } };
+}
+
+export default function Home({ pokemons }: any) {
+    return (
+        <div>
+            <h1>PokeNext</h1>
+            <ul>
+                {pokemons.map((pokemon: any) => {
+                    return <li key={pokemon.id}>{pokemon.name}</li>;
+                })}
+            </ul>
+        </div>
+    );
 }
